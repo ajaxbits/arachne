ca09fa2d0028f0680fa762eb345f5c25cc112589
Alex Jackson <git@ajaxbits.com>
2025-08-20 13:22:18 -0500

    fixes

--- disks.nix
+++ disks.nix
@@ -235,16 +235,19 @@ in
     };
   };
 
+  age.secretsDir = "${persistentPath}/agenix";
+  age.secretsMountPoint = "${persistentPath}/agenix.d";
+
   services.openssh = {
     hostKeys = [
       {
         type = "ed25519";
-        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        path = "${persistentPath}/etc/ssh/ssh_host_ed25519_key";
       }
       {
         type = "rsa";
         bits = 4096;
-        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        path = "${persistentPath}/etc/ssh/ssh_host_rsa_key";
       }
     ];
   };
